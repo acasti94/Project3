6c39315b8691a288e37fd340a0a7825c0593b915
Adelson Castillo <adelsonramon@gmail.com>
2024-09-15 14:33:43 -0500

    Changed main.js file

--- src/main.js
+++ src/main.js
@@ -3,3 +3,37 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+new Vue({
+    el: '#app',
+    data: {
+        timeInput: '',    
+        timeLeft: 0,      
+        interval: null    
+    },
+    methods: {
+        startCountdown() {
+            if (this.timeInput > 60 || this.timeInput <= 0) {
+                alert('Please enter a time between 1 and 60 seconds.');
+                return;
+            }
+
+            this.timeLeft = this.timeInput;
+            clearInterval(this.interval); 
+
+            this.interval = setInterval(() => {
+                this.timeLeft--;
+
+                if (this.timeLeft <= 0) {
+                    clearInterval(this.interval);
+                    alert('Time is up!');
+                }
+            }, 1000);
+        },
+        resetCountdown() {
+            clearInterval(this.interval); 
+            this.timeInput = '';          
+            this.timeLeft = 0;            
+        }
+    }
+});
